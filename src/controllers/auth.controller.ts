4dd3c9143c3a63c47d5cc1bd80282db086f1f1c1
Rana Aldosari <rana@example.com>
2025-06-29 20:07:49 +0300

    create signUp & signIn functions

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,39 @@
+import { RequestHandler } from "express"
+import { validationResult } from "express-validator"
+import { CREATED, OK } from "../utils/http-status"
+// handel with signUp function
+export const signup: RequestHandler = async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() })
+    return
+  }
+
+  try {
+    const { email, password } = req.body
+// create JWT
+    const token = "dummy-signup-token" 
+    res.status(CREATED).json({ token })
+  } catch (err) {
+    next(err)
+  }
+}
+// handel with signIn function
+export const signin: RequestHandler = async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() })
+    return
+  }
+
+  try {
+    const { email, password } = req.body
+// check of users
+ const token = "dummy-signin-token" 
+
+    res.status(OK).json({ token })
+  } catch (err) {
+    next(err)
+  }
+}
+
