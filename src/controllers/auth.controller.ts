938466e1cf5d0d4b5500a8a3bdb933f39a49d214
Rana Aldosari <rana@example.com>
2025-06-29 20:24:12 +0300

    initialize auth controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express"
 import { validationResult } from "express-validator"
 import { CREATED, OK } from "../utils/http-status"
+import { AuthService } from "../services/auth.service"
 // handel with signUp function
 export const signup: RequestHandler = async (req, res, next) => {
   const errors = validationResult(req)
@@ -8,16 +9,17 @@ export const signup: RequestHandler = async (req, res, next) => {
     res.status(400).json({ errors: errors.array() })
     return
   }
-
   try {
-    const { email, password } = req.body
-// create JWT
-    const token = "dummy-signup-token" 
+    const { token } = await AuthService.signup(
+      req.body.email,
+      req.body.password
+    )
     res.status(CREATED).json({ token })
   } catch (err) {
     next(err)
   }
 }
+
 // handel with signIn function
 export const signin: RequestHandler = async (req, res, next) => {
   const errors = validationResult(req)
@@ -25,21 +27,20 @@ export const signin: RequestHandler = async (req, res, next) => {
     res.status(400).json({ errors: errors.array() })
     return
   }
-
-  try {
+ try {
     const { email, password } = req.body
-// check of users
- const token = "dummy-signin-token" 
-
+    const { token } = await AuthService.login(email, password)
     res.status(OK).json({ token })
   } catch (err) {
     next(err)
   }
 }
+
 // handel with signOut function
 export const signout: RequestHandler = (req, res, next) => {
   try {
-// if success
+    const authHeader = req.headers.authorization
+    AuthService.logout(authHeader)
     res.status(OK).json({ success: true, message: "Signed out successfully" })
   } catch (err) {
     next(err)
