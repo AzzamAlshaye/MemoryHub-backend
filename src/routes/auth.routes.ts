9ac8af5d28f6d0be5782cf47631b27785f896e87
Rana Aldosari <rana@example.com>
2025-06-29 19:59:19 +0300

    intialize the routers

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express"
+import { body } from "express-validator"
+
+const router = Router()
+// signUp-router
+router.post("/signup",)
+// signIn-router
+router.post("/login",)
+// signOut-router
+router.post("/logout")
+
+export default router
