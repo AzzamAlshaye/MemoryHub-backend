a47a29d9b55158888714b8ead1ebc8bd53939eb9
Rana Aldosari <rana@example.com>
2025-07-01 21:54:00 +0300

    update likes controler,service,model

--- src/models/like.model.ts
+++ src/models/like.model.ts
@@ -1,24 +1,52 @@
 import { Schema, model, Document, Types } from "mongoose"
-// create inerafce type
+// create interface
 export interface LikeDocument extends Document {
-  id: string
-  user: Types.ObjectId
-  targetType: "pin" | "comment"
-  targetId: Types.ObjectId
-  type: "like" | "dislike"
+  _id: Types.ObjectId 
+  user: Types.ObjectId 
+  targetType: "pin" | "comment" 
+  targetId: Types.ObjectId 
+  type: "like" | "dislike" 
   createdAt: Date
   updatedAt: Date
 }
 // create schema
 const likeSchema = new Schema<LikeDocument>(
   {
-    id: { type: String, default: () => `like_${Date.now()}` },
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    targetType: { type: String, enum: ["pin", "comment"], required: true },
-    targetId: { type: Schema.Types.ObjectId, required: true },
-    type: { type: String, enum: ["like", "dislike"], required: true },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    targetType: {
+      type: String,
+      enum: ["pin", "comment"],
+      required: true,
+    },
+    targetId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["like", "dislike"],
+      required: true,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform(doc, ret) {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.__v
+      },
+    },
+  }
 )
+// get by id
+likeSchema.virtual("id").get(function (this: LikeDocument) {
+  return this._id.toHexString()
+})
 
 export const LikeModel = model<LikeDocument>("Like", likeSchema)
